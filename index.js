f902f507f664f10d9b43f179cc3382a30c84c34e
Asad9340 <asadulimran1999@gmail.com>
2024-04-28 02:23:46 +0600

    added specific subcategory data fetch

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 //middleware
@@ -12,8 +11,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Artistic Alchemy!');
-})
-
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kj2w8eq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,21 +27,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    const UserCraftCollection = client.db('ArtisticAlchemy').collection('userCraft');
-    const subcategoryCollection = client.db('ArtisticAlchemy').collection('categories');
+    const UserCraftCollection = client
+      .db('ArtisticAlchemy')
+      .collection('userCraft');
+    const subcategoryCollection = client
+      .db('ArtisticAlchemy')
+      .collection('categories');
 
     app.get('/categories', async (req, res) => {
+      const subcategory = req.params.subcategory;
+      const query = { subcategory_name: subcategory };
       const cursor = subcategoryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
-
+    });
 
     app.get('/allArtCraft', async (req, res) => {
-                const cursor = UserCraftCollection.find();
-                const result = await cursor.toArray();
-                res.send(result);
-        });
+      const cursor = UserCraftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.get('/details/:id', async (req, res) => {
       const id = req.params.id;
@@ -60,53 +63,51 @@ async function run() {
 
     app.get('/myCraft/:email', async (req, res) => {
       const email = req.params.email;
-      const query = { email:email };
+      const query = { email: email };
       const cursor = UserCraftCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-        app.post('/addCraft', async (req, res) => {
-          const newCraft = req.body;
-          console.log(newCraft);
-          const result = await UserCraftCollection.insertOne(newCraft);
-          res.send(result);
-        });
+    app.post('/addCraft', async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await UserCraftCollection.insertOne(newCraft);
+      res.send(result);
+    });
 
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const changedCraft = req.body;
+      const updatedCraft = {
+        $set: {
+          image: changedCraft.image,
+          item_name: changedCraft.item_name,
+          subcategory_name: changedCraft.subcategory_name,
+          description: changedCraft.description,
+          price: changedCraft.price,
+          rating: changedCraft.rating,
+          customization: changedCraft.customization,
+          processing_time: changedCraft.processing_time,
+          stock_status: changedCraft.stock_status,
+        },
+      };
+      const result = await UserCraftCollection.updateOne(
+        filter,
+        updatedCraft,
+        options
+      );
+      res.send(result);
+    });
 
-        app.put('/update/:id', async (req, res) => {
-          const id = req.params.id;
-          const filter = { _id: new ObjectId(id) };
-          const options = { upsert: true };
-          const changedCraft = req.body;
-          const updatedCraft = {
-            $set: {
-              image: changedCraft.image,
-              item_name: changedCraft.item_name,
-              subcategory_name: changedCraft.subcategory_name,
-              description: changedCraft.description,
-              price: changedCraft.price,
-              rating: changedCraft.rating,
-              customization: changedCraft.customization,
-              processing_time: changedCraft.processing_time,
-              stock_status: changedCraft.stock_status,
-            },
-          };
-          const result = await UserCraftCollection.updateOne(
-            filter,
-            updatedCraft,
-            options
-          );
-          res.send(result);
-        });
-
-
-        app.delete('/delete/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const result = await UserCraftCollection.deleteOne(query);
-          res.send(result);
-        });
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await UserCraftCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
@@ -118,7 +119,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log('Project is listening on port ', port);
